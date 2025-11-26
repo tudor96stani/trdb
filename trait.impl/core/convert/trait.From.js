72aa0c68b677fecb00505dca78002a0a7207b4ae
tudor96stani <tudor96stani@users.noreply.github.com>
2025-11-26 19:24:06 +0000

    deploy: 9f3944db6bf14c70d689664f8af3af087c2de2d7

--- trait.impl/core/convert/trait.From.js
+++ trait.impl/core/convert/trait.From.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["binary_helpers",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/1.91.1/core/array/struct.TryFromSliceError.html\" title=\"struct core::array::TryFromSliceError\">TryFromSliceError</a>&gt; for <a class=\"enum\" href=\"binary_helpers/bin_error/enum.BinaryError.html\" title=\"enum binary_helpers::bin_error::BinaryError\">BinaryError</a>"]]],["page",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;BinaryError&gt; for <a class=\"enum\" href=\"page/page_error/enum.PageError.html\" title=\"enum page::page_error::PageError\">PageError</a>"]]]]);
+    var implementors = Object.fromEntries([["binary_helpers",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/1.91.1/core/array/struct.TryFromSliceError.html\" title=\"struct core::array::TryFromSliceError\">TryFromSliceError</a>&gt; for <a class=\"enum\" href=\"binary_helpers/bin_error/enum.BinaryError.html\" title=\"enum binary_helpers::bin_error::BinaryError\">BinaryError</a>"]]],["page",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"enum\" href=\"binary_helpers/bin_error/enum.BinaryError.html\" title=\"enum binary_helpers::bin_error::BinaryError\">BinaryError</a>&gt; for <a class=\"enum\" href=\"page/page_error/enum.PageError.html\" title=\"enum page::page_error::PageError\">PageError</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[492,298]}
+//{"start":57,"fragment_lengths":[492,430]}
