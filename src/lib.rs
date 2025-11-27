8945b87e76cd0c3bb2004d116527f17dc90779eb
tudor96stani <tudor96stani@gmail.com>
2025-11-27 12:09:21 +0200

    Moved main crate to root folder

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,8 @@
 //! # TRDB Server
 //!
-//! This is the main entry point for the **TRDB** database server.
+//! This is the main crate for the **TRDB** database server.
 //!
 //! The server is composed of multiple internal components organized under
 //! the `/crates` directory of this workspace:
 //!
 //! - `/storage`: Core storage engine handling data persistence and retrieval.
-//!
-//! This binary is the main executable for the database engine.
-fn main() {
-    println!("Starting up TRDB server...");
-}
---
+++ src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("Starting up TRDB server...");
+}
